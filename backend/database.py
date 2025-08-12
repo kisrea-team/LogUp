de98fc65c5606f934be121f5fb6e0633596e16a9
Onlook <git@onlook.com>
2025-08-12 23:00:39 +0800

    前端遇到了一个问题

--- backend/database.py
+++ backend/database.py
@@ -7,11 +7,11 @@ load_dotenv()
 
 class Database:
     def __init__(self):
-        self.host = os.getenv('DB_HOST', 'localhost')
-        self.port = os.getenv('DB_PORT', 3306)
-        self.user = os.getenv('DB_USER', 'root')
-        self.password = os.getenv('DB_PASSWORD', '')
-        self.database = os.getenv('DB_NAME', 'project_updates')
+        self.host = '127.0.0.1'
+        self.port = 3306
+        self.user = 'root'
+        self.password = 'zhangkuo'
+        self.database = 'project_updates'
         self.connection = None
 
     def connect(self):
@@ -21,7 +21,7 @@ class Database:
                 port=self.port,
                 user=self.user,
                 password=self.password,
-                database=self.database
+                database=self.database,
             )
             return self.connection
         except Error as e:
